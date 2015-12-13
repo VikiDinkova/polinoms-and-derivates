4d825bfb47941203e87fbc34ccfbc4625c37f28b
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-12-13 23:27:23 +0200

    init of class derivative

--- derivative.py
+++ derivative.py
@@ -1,4 +1,5 @@
 class Derivative:
     """docstring for Derivative"""
-    def __init__(self):
-        pass
+    def __init__(self, polynom):
+        self.polynom = polynom
+        self.derivative = "Not calculated yet"
